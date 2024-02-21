4bb026b110d6815cf19f95c9a2c1e2ccb24e180a
elisafer02 <efernandeziquise@gmail.com>
2024-02-20 23:43:54 -0500

    added

--- juego.py
+++
@@ -1,9 +0,0 @@
-import random 
-
-listado = ['computadora','programador', 'python', 'javascript', 'nodejs']
-clave = random.choice(listado)
-
-intentos = int (len(clave))
-while intentos != 0:
-    print (intentos)
-    intentos -= 1
